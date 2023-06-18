2f5971fb67deaca49dd6ea242b1c4d12caafe037
Tingting Liao <45743512+TingtingLiao@users.noreply.github.com>
2023-06-18 21:18:36 +0800

    Delete lib directory

--- lib/data/__init__.py
+++
@@ -1,5 +0,0 @@
-from .TrainDataset import TrainDataset
-from .EvalDataset import EvalDataset
-from .TrainDataset import TrainDataset
-from .Evaluator import Evaluator
-from .THumanDataset import THumanDataset
